a1d84ebfc3726fbb35458e874917350197781421
ephym6 <ephy.macharia.mbuu@gmail.com>
2025-04-26 23:55:37 +0300

    Update sumOfDigits.py

--- sumOfDigits.py
+++ sumOfDigits.py
@@ -1,1 +1,10 @@
+def sumOfDigits(n):
+  total = 0
+  for digit in str(n):
+    total += int(digit)
+  return total
 
+#test case
+if __name__ == "__main__":
+  n = int(input("Enter a number: "))
+    print(f"Sum of digits: {sum_digits(n)}")
