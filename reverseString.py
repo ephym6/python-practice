17c68f0a0b3d6c1da84d6596e18c2b9601e93425
ephym6 <ephy.macharia.mbuu@gmail.com>
2025-04-26 23:49:21 +0300

    Update reverseString.py

--- reverseString.py
+++ reverseString.py
@@ -1,1 +1,10 @@
+def reverseString(r):
+  reversed_str = ""
+  for i in range(len(s) - 1, -1, -1):
+        reversed_str += r[i]
+    return reversed_str
+
+if __name__ == "__main__":
+  r = input("Enter a string: ")
+    print("Reversed string: ", reverse_string(r))
 
