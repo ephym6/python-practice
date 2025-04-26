ae29ef05ec4e04a8eb067e98bdcc0127854048ff
ephym6 <ephy.macharia.mbuu@gmail.com>
2025-04-26 23:42:12 +0300

    Update and rename evenAndOdd.py to evenOrOdd.py

---
+++ evenOrOdd.py
@@ -0,0 +1,11 @@
+def evenOrOdd(num):
+  if num % 2 == 0
+  return "Even"
+  else 
+  return "Odd"
+
+#example
+if __name__ == "__main__":
+  num = int(input("Enter a number: "))
+    print(f"{num} is {evenOrOdd(num)}")
+
