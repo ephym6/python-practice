c1b006a614bf603e04276b3443fa36e22090ea8c
ephym6 <ephy.macharia.mbuu@gmail.com>
2025-04-26 23:36:11 +0300

    Create sumInAList.py

---
+++ sumInAList.py
@@ -0,0 +1,10 @@
+#function for putting elements in list
+def sumInList(elements):
+  sumInList = 0
+  for i in elements 
+  elements += i
+
+#sum function
+if __name__ = "__main__"
+elements = [1, 2, 3, 4, 5]
+print("Sum of elements in list: ", sum(elements))
